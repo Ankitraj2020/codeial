dc7d1317f31660afee6c178ba7b737d0e1b1bc3b
Ankitraj2020 <78090782+Ankitraj2020@users.noreply.github.com>
2023-05-19 16:02:23 +0530

    add sign-in anf sign-up detail db

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,62 @@
+const passport = require('passport');
+
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+//authentication using passport
+passport.use(new LocalStrategy({
+    usernameField:'email'
+},
+function(email,password,done){
+//find a user and establish the identity
+User.findOne({email:email},function(err,user){
+    if(err){
+        console.log('Error in finding user ---> passport');
+        return done(err);
+    }
+    if(!user||user.password!=password){
+        console.log('Invalid username/password');
+        return done(null,false);
+    }
+    return done(null,user);
+});
+}
+));
+
+
+
+ passport.serializeUser(function(user,done){
+    done(null,user.id);
+ });
+ passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if(err){
+            console.log('Error in finding user---> passport');
+            return done(err);
+        }
+        return done(null,user);
+    });
+ });
+
+
+
+ //check if the user is authenticated
+ passport.checkAuthentication = function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    return res.redirect('/usres/sign-in');
+ }
+
+ passport.setAuthenticatedUser = function(req,res,next){
+    //setting user detail in ejs file
+    if(req.isAuthenticated()){
+        res.locals.user  = req.user;
+    }
+    next();
+ }
+
+
+ module.exports = passport;
+
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,11 +9,19 @@ module.exports.profile = function(req, res){
 
 
 module.exports.signUp= function(req,res){
+
+   if(req.isAuthenticated()){
+      return res.redirect('/users/profile');
+   }
    return res.render('user_sign_up',{
       title:"codeial | sign up"
    });
 }
 module.exports.signIn= function(req,res){
+
+   if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+   }
    return res.render('user_sign_in',{
       title:"codeial | sign in"
    });
@@ -39,6 +47,6 @@ module.exports.create= function(req,res){
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req,res){
-   //to do
+   return res.redirect('/');
 }
 
