b44c84b7d61d705c55a38cc675634028bc87e446
Ankitraj2020 <78090782+Ankitraj2020@users.noreply.github.com>
2023-05-19 19:22:36 +0530

    add sign out

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require('./config/mongoose');
 const session=require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -24,7 +25,7 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
-
+//mongo store is udes to store the session cookie in db
 app.use(session({
     name:'codeial',
     //TODO change before deployment in production mode
@@ -33,7 +34,17 @@ app.use(session({
     resave:false,
     cookie:{
         maxAge:(1000*60*100)
+    },
+    store:  new MongoStore(
+        {
+         mongooseConnection: db,
+         autoRemove :'disabled'   
+        
+    },
+    function(err){
+        console.log(err ||'coonect mongodb setup ok');
     }
+    )
 
 }));
 app.use(passport.initialize());
