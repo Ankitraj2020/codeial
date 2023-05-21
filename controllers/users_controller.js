4c8ddff17d8e8847c0a06e7a740567464d4ef20c
Ankitraj2020 <78090782+Ankitraj2020@users.noreply.github.com>
2023-05-21 18:40:54 +0530

    add update profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,7 +10,15 @@ module.exports.profile = function(req, res){
    });
    
 }
-
+module.exports.update = function(req,res){
+   if(req.user.id==req.params.id){
+      User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+         return res.redirect('back');
+      });
+   }else{
+      return res.status(401).send('unauthorized');
+   }
+}
 
 
 module.exports.signUp= function(req,res){
@@ -25,7 +33,7 @@ module.exports.signUp= function(req,res){
 module.exports.signIn= function(req,res){
 
    if(req.isAuthenticated()){
-       return res.redirect('/users/profile/');
+       return res.redirect('/users/profile');
    }
    return res.render('user_sign_in',{
       title:"codeial | sign in"
@@ -52,7 +60,7 @@ module.exports.create= function(req,res){
 }
 //sign in and create a session for the user
 module.exports.createSession = function(req,res){
-   return res.redirect('/users/profile');
+   return res.redirect('/');
 }
 
 
